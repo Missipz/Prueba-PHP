a9695c996d9e84c6a781e245ccc279f1201dc8a8
Missipz <maquinavirtualmac1@gmail.com>
2021-02-28 21:45:41 -0600

    hola

---
+++ Clase1pz/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> PZ | Login</title>
+	<style type="text/css">
+		/*
+		button{
+			background-color: #8adba0;
+			color: white;
+		}
+
+		label{
+			color:orange;
+		}
+		.rosa{
+			color:#eba2d2;
+		}
+		#btnpass{
+			background: #f72905;
+
+		}*/
+	</style>
+<link rel="stylesheet" type="text/css" href="css/stilos.css">
+</head>
+<body>
+<label>
+	<label>Usuario</label>
+	<input type="" name="">
+	<label>Contraseña</label>
+	<input type="" name="">
+	<button  class="rosa">Ingresar</button>
+	<button>Registro</button>
+	<button id="btnpass">Recordar contraseña</button>
+	<button class="ClaseExterna">Facebook</button>
+</label>
+</body>
+</html>
